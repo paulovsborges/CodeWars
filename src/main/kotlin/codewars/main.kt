675543cb58f0d1ef03622f9be8a829711ae5b0cc
Borges <borgesgamepay@gmail.com>
2023-07-16 14:16:28 -0300

    feat: add fibonacci sequence

--- src/main/kotlin/codewars/main.kt
+++ src/main/kotlin/codewars/main.kt
@@ -1,5 +1,13 @@
 package codewars
 
-fun main(){
+fun main() {
+    fibonacci(0, 1)
+}
 
+private fun fibonacci(first: Int, second: Int) {
+    val result = first + second
+    println("$first + $second = $result")
+    if (result < 100) {
+        fibonacci(second, result)
+    }
 }
